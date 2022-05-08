88c8ee92c50182a9f96f8c61a650055f2167e30b
Pedro <pedrodewaal@gmail.com>
2022-05-08 20:30:50 -0300

    NLW 8ª edição

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "d1b8662613b4ff",
+      pass: "8527e749408241"
+    }
+  });
+
+export class NodemailerMailAdapter implements MailAdapter {
+    async sendMail({ subject, body }: SendMailData) {
+        await transport.sendMail({
+            from: 'Equipe Feedget <oi@feedget.com>',
+            to: 'Lumen <lumentech1@gmail.com>',
+            subject,
+            html: body,
+        });
+    }
+}
